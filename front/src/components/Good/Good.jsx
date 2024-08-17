f1a3a35231ab7ec5872cd2e80cd348b627dad798
fallenreds <artur.khobotkov@nure.ua>
2024-08-18 00:16:19 +0300

    Base funtional work

--- front/src/components/Good/Good.jsx
+++ front/src/components/Good/Good.jsx
@@ -1,69 +1,95 @@
 import React, {useEffect, useState} from 'react';
-import classes from './Good.module.css'
+import ImageGallery from 'react-image-gallery';
+import Modal from 'react-modal';
+import 'react-image-gallery/styles/css/image-gallery.css';
+import classes from './Good.module.css';
 import {get_discount, postShoppingCart} from "../../hooks/api";
 import {useNavigate} from "react-router-dom";
 
+Modal.setAppElement('#root'); // Set the root element for accessibility
 
 const Good = (props) => {
-    const uid = props.uid
-    const buyButton = <button onClick={addToShoppingCart}>Придбати</button>
-    const toShoppingCartButton = <button onClick={()=>router('/shcart')} style={{background: "rgba(215, 125, 72, 0.3)"}}>До кошику</button>
-    const cantBuyButton = <button style={{background: "rgba(255,0,0,0.13)", borderColor:"rgba(255,0,0, 1)"}}>Немає в наявності</button>
+    const uid = props.uid;
+    const buyButton = <button onClick={addToShoppingCart}>Придбати</button>;
+    const toShoppingCartButton = <button onClick={()=>router('/shcart')} style={{background: "rgba(215, 125, 72, 0.3)"}}>До кошику</button>;
+    const cantBuyButton = <button style={{background: "rgba(255,0,0,0.13)", borderColor:"rgba(255,0,0, 1)"}}>Немає в наявності</button>;
 
-    const [buttonState, setButton] = useState(buyButton)
-    const [cartState, setCart] = useState(props.carts)
-    const [goodState, setGood] = useState(props.good)
-    // eslint-disable-next-line
+    const [buttonState, setButton] = useState(buyButton);
+    const [cartState, setCart] = useState(props.carts);
+    const [goodState, setGood] = useState(props.good);
+    const [isGalleryOpen, setGalleryOpen] = useState(false);
+    const [galleryIndex, setGalleryIndex] = useState(0);
+
     useEffect(()=>{
-        setGood(props.good)
+        setGood(props.good);
+    },[props.good]);
 
-    },[props.good])
-    // eslint-disable-next-line
     useEffect(()=>{
-        setCart(props.carts)
+        setCart(props.carts);
+    },[props.carts]);
 
-    },[props.carts])
-
-    const router = useNavigate()
-    let image = goodState.image[0]
+    const router = useNavigate();
+    let image = goodState.image[0];
     if(!image){
-        image ='https://t3.ftcdn.net/jpg/04/34/72/82/360_F_434728286_OWQQvAFoXZLdGHlObozsolNeuSxhpr84.jpg'
+        image ='https://t3.ftcdn.net/jpg/04/34/72/82/360_F_434728286_OWQQvAFoXZLdGHlObozsolNeuSxhpr84.jpg';
     }
 
-    // eslint-disable-next-line
     useEffect(()=>{
         if (props.carts.includes(goodState.id)){
-            setButton(toShoppingCartButton)
+            setButton(toShoppingCartButton);
         }
         else if(goodState.residue===0){
-            setButton(cantBuyButton)
+            setButton(cantBuyButton);
         }
         else {
-            setButton(buyButton)
+            setButton(buyButton);
         }
-    // eslint-disable-next-line
-    },[goodState,props.carts])
-
+    },[goodState,props.carts]);
 
     function addToShoppingCart (){
-        setButton(toShoppingCartButton)
-        postShoppingCart(uid, goodState.id)
-
+        setButton(toShoppingCartButton);
+        postShoppingCart(uid, goodState.id);
     }
 
+    const handleImageClick = () => {
+        setGalleryIndex(0); // Set index to 0 to open on the first image
+        setGalleryOpen(true);
+    };
 
+    const images = goodState.image.map(img => ({
+        original: img,
+        thumbnail: img,
+    }));
+
+    const closeModal = () => {
+        setGalleryOpen(false);
+    };
 
     return (
         <div className={classes.post}>
+            <Modal
+                isOpen={isGalleryOpen}
+                onRequestClose={closeModal}
+                contentLabel="Image Gallery"
+                className={classes.modalContent}
+                overlayClassName={classes.modalOverlay}
 
-            <div className={classes.imageContainer}>
+            >
+                <ImageGallery
+                    items={images}
+                    startIndex={galleryIndex}
+                    onClose={closeModal}
+                    showPlayButton={false}
+                    showThumbnails={true}
+                    sho
+                    useBrowserFullscreen={true}
+                />
+            </Modal>
 
+            <div className={classes.imageContainer} onClick={handleImageClick}>
                 <img className={classes.blurIMG} alt='Фото' src={image}/>
                 <img className={classes.finalIMG} alt='Фото' src={image}/>
-
             </div>
-
-
 
             <div className={classes.title}>
                 {goodState.title}<p/>
@@ -76,7 +102,6 @@ const Good = (props) => {
             </div>
 
             {buttonState}
-
 
 
         </div>
