82d24c6bc377c267b96bc1c439407bfc08553ae7
fallenreds <artur.khobotkov@nure.ua>
2024-08-17 21:37:35 +0300

    Check quantity error

--- front/src/components/ShopingCart/ShopingCart.jsx
+++ front/src/components/ShopingCart/ShopingCart.jsx
@@ -6,81 +6,78 @@ import {deleteShoppingCart, getOrderSuma, updateShoppingCart} from "../../hooks/
 const test_price = 298792
 const prod_price = 284727
 const ShopingCart = (props) => {
-
     const [count, setCount] = useState(props.cart.count);
-    const good = props.goodsState.filter(item=>item.id===props.cart.good_id)[0]
-    const [price, setPrice] = useState(good.price[prod_price]*count);
-    let cart_id = props.cart.id
-
-    useEffect(()=>{
-        setPrice()
-    })
-    function updateSuma(){
-        getOrderSuma(props,props.uid)
-    }
-    function deleteCart(){
-        props.setOrderSuma(props.orderSumaState-good.price[prod_price]*count)
-        deleteShoppingCart(cart_id)
-        props.removeCart(cart_id)
-    }
-
-    function incrementCount() {
-        if(count+1 <= good.residue){
-            setCount(count+1)
-            updateShoppingCart(cart_id, count+1)
-
-            props.setOrderSuma(props.orderSumaState+good.price[prod_price])
-        }
-    }
-    function decrementCount() {
-        if(count-1>0){
-            setCount(count-1)
-            updateShoppingCart(cart_id, count-1)
-            props.setOrderSuma (props.orderSumaState-good.price[prod_price])
-        }
-
-    }
-
-
-
+    const good = props.goodsState.find(item => item.id === props.cart.good_id);
+    const [price, setPrice] = useState(good.price[prod_price] * count);
+    const [error, setError] = useState(false);
     let image = good.image[0]
     if(!image){
         image ='https://t3.ftcdn.net/jpg/04/34/72/82/360_F_434728286_OWQQvAFoXZLdGHlObozsolNeuSxhpr84.jpg'
     }
+
+    useEffect(() => {
+        checkErrorState();
+    }, [count, good.residue]);
+    function isQuantityError(){
+        return count>good.residue
+    }
+
+    function checkErrorState() {
+        const errorState = isQuantityError();
+        if (errorState !== error) {
+            setError(errorState);
+            props.onErrorChange(props.index, errorState);
+        }
+    }
+
+
+    const incrementCount = () => {
+        if (count + 1 <= good.residue) {
+            setCount(count + 1);
+            updateShoppingCart(props.cart.id, count + 1);
+            props.setOrderSuma(props.orderSumaState + good.price[prod_price]);
+        }
+    };
+
+    const decrementCount = () => {
+        if (count - 1 > 0) {
+            setCount(count - 1);
+            updateShoppingCart(props.cart.id, count - 1);
+            props.setOrderSuma(props.orderSumaState - good.price[prod_price]);
+        }
+    };
+
+    const deleteCart = () => {
+        props.setOrderSuma(props.orderSumaState - good.price[prod_price] * count);
+        deleteShoppingCart(props.cart.id);
+        props.removeCart(props.cart.id);
+        checkErrorState()
+    };
+    checkErrorState()
     return (
         <div className={classes.shoppingCart}>
             <div className={classes.imageContainer}>
-
-                <img className={classes.blurIMG} alt='Фото' src={image}/>
-                <img className={classes.finalIMG} alt='Фото' src={image}/>
-
+                <img className={classes.blurIMG} alt='Фото' src={image  || 'default_image_url'} />
+                <img className={classes.finalIMG} alt='Фото' src={image  || 'default_image_url'} />
             </div>
             <div className={classes.content}>
-
-
-            <div className={classes.info}>
-                <div className={classes.maintext}>{good.title}</div>
-                <div className={classes.price}>{good.price[284727]}₴</div>
-                <div className={classes.counter}>
-                    <button onClick={decrementCount}>-</button>
-                    <span>{count}</span>
-                    <button onClick={incrementCount}>+</button>
-                </div>
-                <div >В наявності: {good.residue} шт</div>
-
-                <div className={classes.finalprice}>
-
-                    <div className={classes.finaltext}>
-                        До сплати:
+                <div className={classes.info}>
+                    <div className={classes.maintext}>{good.title}</div>
+                    <div className={classes.price}>{good.price[prod_price]}₴</div>
+                    <div className={classes.counter}>
+                        <button onClick={decrementCount}>-</button>
+                        <span>{count}</span>
+                        <button onClick={incrementCount}>+</button>
                     </div>
-
-                    {good.price[prod_price]*count} ₴
+                    <div style={ {color: error ? 'red' : 'black'} }>В наявності: {good.residue} шт</div>
+                    <div className={classes.finalprice}>
+                        <div className={classes.finaltext}>До сплати:</div>
+                        {good.price[prod_price] * count} ₴
+                    </div>
                 </div>
             </div>
-            </div>
-
             <button
-                style={{color: "red", marginLeft: "auto", marginBottom:"auto", background:"None", border: "None"}}
+                style={{color: "red", marginLeft: "auto", marginBottom: "auto", background: "None", border: "None"}}
                 onClick={deleteCart}
             >
                 Видалити
@@ -88,5 +85,4 @@ const ShopingCart = (props) => {
         </div>
     );
 };
-
-export default ShopingCart;
+export default ShopingCart
--- front/src/components/ShopingCartList/ShopingCartList.jsx
+++ front/src/components/ShopingCartList/ShopingCartList.jsx
@@ -1,130 +1,151 @@
-import React, {useEffect, useState} from 'react';
+import React, { useEffect, useState } from 'react';
 import classes from "./ShopingCartList.module.css";
 import ShopingCart from "../ShopingCart/ShopingCart";
-import {getGoods, getShoppingCart, getOrderSuma, get_discount, checkAuth} from "../../hooks/api";
-import {useNavigate} from "react-router-dom";
+import { getGoods, getShoppingCart, getOrderSuma, get_discount, checkAuth } from "../../hooks/api";
+import { useNavigate } from "react-router-dom";
 
-const prod_price = 284727
 const ShopingCartList = (props) => {
-    const router = useNavigate()
-    const uid = props.uid
-    const [shoppingCartState, setShoppingCart] = useState([])
-    const [orderSumaState, setOrderSuma]= useState(0)
-    const setGoods = props.setGoods
-    const goodsState = props.goodsState
-    const [authenticated, setAuthenticated] = useState()
-    const [client, setClient] = useState()
-    const [discount, setDiscount] = useState(0)
+    const router = useNavigate();
+    const uid = props.uid;
+    const [shoppingCartState, setShoppingCart] = useState([]);
+    const [orderSumaState, setOrderSuma] = useState(0);
+    const setGoods = props.setGoods;
+    const goodsState = props.goodsState;
+    const [authenticated, setAuthenticated] = useState(false);
+    const [client, setClient] = useState(null);
+    const [discount, setDiscount] = useState(0);
+    const [childErrors, setChildErrors] = useState({});
+    const [parentError, setParentError] = useState(false);
 
-    useEffect(()=>{
-        checkAuth({uid}).then(
-            response=>{
-                if(response.status===true){
-
-                    setAuthenticated(true)
-                    setClient(response.client_data)
-                }
+    useEffect(() => {
+        checkAuth({ uid }).then(response => {
+            if (response.status === true) {
+                setAuthenticated(true);
+                setClient(response.client_data);
             }
-        )
-    },[])
+        });
+    }, [uid]);
 
+    useEffect(() => {
+        getGoods({ setGoods });
+    }, [setGoods]);
 
-    useEffect(()=>{
-       getGoods({setGoods})
-    },[])
+    useEffect(() => {
+        getShoppingCart({ setShoppingCart }, uid);
+    }, [uid]);
 
-    useEffect(()=>{
-        getShoppingCart({setShoppingCart}, uid)
-    },[])
+    useEffect(() => {
+        getOrderSuma({ setOrderSuma }, uid);
+    }, [uid]);
 
-    useEffect(()=>{
-        getOrderSuma({setOrderSuma},uid)
-    },[])
-    // function getGoodById(good_id){
-    //     return goodsState.find((item)=>{
-    //
-    //         if(item.id===good_id){
-    //
-    //             return item
-    //         }
-    //
-    //     })
-    // }
-    // function getOrderSum (){
-    //     let suma = 0
-    //     shoppingCartState.map((item)=>{
-    //         suma = suma+getGoodById(item.good_id).price[prod_price]*item.count
-    //     })
-    //     return suma
-    // }
-
-    if(authenticated===true && client){
-        get_discount(client.id).then(
-            response=>{
-                if(response.data.success===true){
-                    console.log(response.data)
-                    setDiscount(response.data.data.procent)
-                    //return <div>Вам доступна знижка у розмірі {response.data.data.procent}%</div>
+    useEffect(() => {
+        if (authenticated && client) {
+            get_discount(client.id).then(response => {
+                if (response.data.success === true) {
+                    setDiscount(response.data.data.procent);
                 }
-            }
-        )
-    }
-    console.log(discount)
-
-    function showDiscount(){
-        if(discount>0){
-            return <div
-                style={
-                        {color:"rgba(19, 189, 50, 1)",
-                        textAlign:"center",
-                        fontWeight:"bold",
-                        fontSize:"3vw",
-                        marginTop:'10px'}}>
-                Вам доступна знижка у розмірі {discount}%
-            </div>
+            });
         }
-    }
+    }, [authenticated, client]);
 
-    function toPay(){
-        if(discount){
-            return <div>
-                <div className={classes.topay} style={{textDecoration:"line-through", color:"red", fontSize:'3vw'}}>Сума до сплати: {orderSumaState} грн</div>
-                <div className={classes.topay}>Сума до сплати: {orderSumaState-orderSumaState/100*discount}грн</div>
-            </div>
+    useEffect(() => {
+        const hasError = Object.values(childErrors).includes(true);
+        setParentError(hasError);
+    }, [childErrors]);
+
+    const handleChildErrorChange = (index, childError) => {
+        setChildErrors(prevErrors => ({
+            ...prevErrors,
+            [index]: childError
+        }));
+    };
+
+    const showDiscount = () => {
+        if (discount > 0) {
+            return (
+                <div
+                    style={{
+                        color: "rgba(19, 189, 50, 1)",
+                        textAlign: "center",
+                        fontWeight: "bold",
+                        fontSize: "3vw",
+                        marginTop: '10px'
+                    }}
+                >
+                    Вам доступна знижка у розмірі {discount}%
+                </div>
+            );
         }
-        return <div className={classes.topay}>Сума до сплати: {orderSumaState}</div>
+        return null;
+    };
 
-    }
-
-
-    function removeCart(id){
-        setShoppingCart(shoppingCartState.filter(item=>item.id !== id))
-    }
-
-    function dsf(){
-        if(orderSumaState!==0) {
-            return <div>{toPay()}
-                <button onClick={() => router('/form')} className={classes.makeOrder}>Зробити замовлення</button></div>
+    const toPay = () => {
+        if (discount) {
+            return (
+                <div>
+                    <div className={classes.topay} style={{ textDecoration: "line-through", color: "red", fontSize: '3vw' }}>
+                        Сума до сплати: {orderSumaState} грн
+                    </div>
+                    <div className={classes.topay}>
+                        Сума до сплати: {orderSumaState - (orderSumaState / 100 * discount)}грн
+                    </div>
+                </div>
+            );
         }
-        else {
-            return <div className={classes.shoppingCartList}>
-                <div className={classes.topay} style={{marginTop:"0px"}}>Ваш кошик порожній🛒</div>
-                <div><button onClick={() => router('/')} className={classes.makeOrder}>Повернутись до товарів</button></div>
-            </div>
+        return <div className={classes.topay}>Сума до сплати: {orderSumaState}</div>;
+    };
+
+    const removeCart = (id) => {
+        setShoppingCart(shoppingCartState.filter(item => item.id !== id));
+    };
+
+    const dsf = () => {
+        if (parentError) {
+            return <div className={classes.topay} style={{ marginTop: "0px", color:"red"}}>
+                Нажаль, товару менше ніж обрано к кошику 😢
+            </div>;
+        } else if (shoppingCartState.length > 0) {
+            return (
+                <div>
+                    {toPay()}
+                    <button onClick={() => router('/form')} className={classes.makeOrder}>
+                        Зробити замовлення
+                    </button>
+                </div>
+            );
+        } else {
+            return (
+                <div className={classes.shoppingCartList}>
+                    <div className={classes.topay} style={{ marginTop: "0px" }}>
+                        Ваш кошик порожній🛒
+                    </div>
+                    <div>
+                        <button onClick={() => router('/')} className={classes.makeOrder}>
+                            Повернутись до товарів
+                        </button>
+                    </div>
+                </div>
+            );
         }
-    }
+    };
 
     return (
-
         <div className={classes.shoppingCartList}>
-
             {showDiscount()}
-            {shoppingCartState.map(info=>
-
-                <ShopingCart cart={info} goodsState={goodsState} orderSumaState={orderSumaState} setOrderSuma={setOrderSuma} uid={uid} removeCart={removeCart}/>
-            )}
+            {shoppingCartState.map((info, index) => (
+                <ShopingCart
+                    key={info.id}
+                    index={index}
+                    cart={info}
+                    goodsState={goodsState}
+                    orderSumaState={orderSumaState}
+                    setOrderSuma={setOrderSuma}
+                    uid={uid}
+                    removeCart={removeCart}
+                    onErrorChange={handleChildErrorChange}
+                />
+            ))}
             {dsf()}
-
         </div>
     );
 };
