f7156040e9ab2f782535695f382d35c6a4989f84
fallenreds <artur.khobotkov@nure.ua>
2025-08-16 17:24:48 +0300

    Add together buy env key.

--- backend_v2/core/apps.py
+++ backend_v2/core/apps.py
@@ -1,14 +1,15 @@
 from django.apps import AppConfig
+
 from core.scheduler import start_scheduler
 
 
 class CoreConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'core'
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "core"
 
     def ready(self):
         import sys
 
-        runner: str = sys.argv[0]
-        if 'runserver' in runner or 'gunicorn' in runner or 'uwsgi' in runner:
+        runner: str = sys.argv[-1]
+        if "runserver" in runner or "gunicorn" in runner or "uwsgi" in runner:
             start_scheduler()
