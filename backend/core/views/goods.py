ae81d1d2ac6de704bfe13be07a1980a5137c054b
fallenreds <artur.khobotkov@nure.ua>
2025-12-05 22:37:47 +0200

    Order goods by availability

--- backend/core/views/goods.py
+++ backend/core/views/goods.py
@@ -2,13 +2,21 @@ from rest_framework import viewsets
 from rest_framework.permissions import AllowAny, IsAdminUser
 
 from core.models import Good, GoodCategory
+from django.db.models import Case, When, Value, IntegerField
 from core.serializers import GoodCategorySerializer, GoodSerializer
 
 from .utils import generate_filterset_for_model
 
 
 class GoodViewSet(viewsets.ModelViewSet):
-    queryset = Good.objects.all()
+    #Получаем все товары и сортируем сначала те что в наличии а потом не в наличии
+    queryset = Good.objects.all().order_by(
+        Case(
+            When(residue__gt=0, then=Value(0)),
+            default=Value(1),
+            output_field=IntegerField()
+        )
+    )
     serializer_class = GoodSerializer
     filterset_class = generate_filterset_for_model(Good)
 
